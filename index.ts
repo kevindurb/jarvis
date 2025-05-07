5490851faa74473d1906448e5c435650992c4b01
Kevin Durbin <kevindurb@fastmail.com>
2025-05-07 10:54:36 -0600

    stop using langchain and start building my own system on ollama

--- index.ts
+++ index.ts
@@ -1,33 +1,5 @@
-import { BaseMessage, HumanMessage, isAIMessage } from "@langchain/core/messages";
-import * as readline from 'node:readline/promises';
-import { app } from "./graph";
+import { Jarvis } from './Jarvis';
 
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
-const threadId = 'asdfasdfasdfasdfd';
-const messageHistory: BaseMessage[] = [];
+const jarvis = new Jarvis();
 
-process.on('SIGINT', () => {
-  process.exit();
-})
-
-while (true) {
-  const input = await rl.question('You: ');
-  const userMessage = new HumanMessage({
-    content: input
-  });
-  messageHistory.push(userMessage);
-
-  const state = await app.invoke(
-    {
-      input,
-    },
-    { configurable: { thread_id: threadId } }
-  );
-
-  for await (const message of state.messages) {
-    if (!messageHistory.includes(message) && isAIMessage(message) && message.text) {
-      console.log('AI: ', message.text)
-    }
-    messageHistory.push(message)
-  }
-}
+jarvis.run();
